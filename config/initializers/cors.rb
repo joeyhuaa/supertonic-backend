f596cd1a31f979fcf17a46e67a1455de5a2c0e6a
Joey Hua <joeyhua17@gmail.com>
2022-10-31 20:26:39 -0700

    add heroku FE app to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'https://supertonic.vercel.app'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://supertonic.vercel.app', 'https://supertonic-frontend.herokuapp.com'
   
     resource '*',
       headers: :any,
