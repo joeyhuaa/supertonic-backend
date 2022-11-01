b978dd5dd2921b39e14f2eb86c9224999ceb521d
Joey Hua <joeyhua17@gmail.com>
2022-11-01 10:30:08 -0700

    add FE domain to session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env === 'production' 
   # todo - change the domain!
-  Rails.application.config.session_store :cookie_store, key: 'supertonic', domain: 'your-frontend-domain'
+  Rails.application.config.session_store :cookie_store, key: 'supertonic', domain: 'https://supertonic-frontend.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: 'supertonic' 
 end
