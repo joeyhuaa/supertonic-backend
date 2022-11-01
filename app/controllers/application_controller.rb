aa4a7967be001ebbe041d0695fa6bf3fb835153f
Joey Hua <joeyhua17@gmail.com>
2022-11-01 13:34:46 -0700

    raise: false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
+  # protect_from_forgery raise: false
+  skip_before_action :verify_authenticity_token, raise: false
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
 
   def login!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 if Rails.env === 'production' 
-  # todo - change the domain!
   Rails.application.config.session_store :cookie_store, key: 'supertonic', domain: 'https://supertonic-frontend.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: 'supertonic' 
